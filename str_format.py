9f846a350906a943afcd1052c716034b94936544
Melaz <dmelanjin@gmail.com>
2014-08-03 19:48:59 +0300

    Some parts forward

---
+++ str_format.py
@@ -0,0 +1,19 @@
+__author__ = 'Dmitry'
+age = 20
+name = 'Swaroop'
+
+print ('{} was {} years old when he wrote this book'.format(name, age))
+print ('Why is {0} playing with that python?'.format(name))
+print (name + ' is ' + str(age) + ' years old')
+
+
+print ('a',end='')
+print ('b')
+
+
+print('''TEST \
+ETST'
+ETSET
+SETET''')
+
+print('new \n w')
